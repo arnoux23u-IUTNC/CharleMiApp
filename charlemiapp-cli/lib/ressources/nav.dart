2bd2442266334195e2a13168ae50c6447d02a309
guillaumearnx <37373941+guillaumearnx@users.noreply.github.com>
2022-01-27 21:26:28 +0100

    move file

--- charlemiapp-cli/lib/ressources/nav.dart
+++ charlemiapp-cli/lib/ressources/nav.dart
@@ -1,5 +1,8 @@
+import 'package:charlemiapp_cli/models/user.dart';
 import 'package:flutter/material.dart';
 import 'package:charlemiapp_cli/widgets/appbar.dart';
+import 'package:provider/provider.dart';
+import 'screens/authscreen.dart';
 
 const midDarkColor = Color(0xFF1c2031);
 const darkColor = Color(0xFF121421);
@@ -8,6 +11,8 @@ const whiteColor = Color(0xFFFFFFFF);
 
 class Nav extends StatefulWidget {
   const Nav({Key? key}) : super(key: key);
+  static bool loading = false;
+  static AppUser? user;
 
   @override
   State<Nav> createState() => _NavState();
@@ -15,11 +20,6 @@ class Nav extends StatefulWidget {
 
 class _NavState extends State<Nav> {
   int _selectedIndex = 0;
-  static const List<Widget> _widgetOptions = <Widget>[
-    /* DiscoverPage(),
-    ChartsPage(),
-    ProfilePage() */
-  ];
 
   void _onItemTapped(int index) {
     setState(() {
@@ -29,14 +29,15 @@ class _NavState extends State<Nav> {
 
   @override
   Widget build(BuildContext context) {
+    Nav.user = Provider.of<AppUser?>(context);
+    var screens = [
+      const HomeScreen(),
+      const ExploreScreen(),
+      ((Nav.user != null) ? const ProfileScreen() : const LoginScreen()),
+    ];
     return Scaffold(
       appBar: MyAppBar(),
-      body: new Column(
-        children: [
-          new ElevatedButton(onPressed: () {}, child: new Text("Connexion")),
-          new ElevatedButton(onPressed: () {}, child: new Text("Inscription"))
-        ],
-      ),
+      body: screens[_selectedIndex],
       bottomNavigationBar: Theme(
         data: ThemeData(
           highlightColor: Colors.transparent,
@@ -48,14 +49,9 @@ class _NavState extends State<Nav> {
           type: BottomNavigationBarType.fixed,
           enableFeedback: true,
           items: const <BottomNavigationBarItem>[
-            BottomNavigationBarItem(
-                icon: Icon(Icons.explore), label: '', tooltip: 'Discover'),
-            BottomNavigationBarItem(
-                icon: Icon(Icons.shopping_basket), label: '', tooltip: 'Order'),
-            BottomNavigationBarItem(
-                icon: Icon(Icons.account_circle),
-                label: '',
-                tooltip: 'Profile'),
+            BottomNavigationBarItem(icon: Icon(Icons.explore), label: '', tooltip: 'Discover'),
+            BottomNavigationBarItem(icon: Icon(Icons.shopping_basket), label: '', tooltip: 'Order'),
+            BottomNavigationBarItem(icon: Icon(Icons.account_circle), label: '', tooltip: 'Profile'),
           ],
           currentIndex: _selectedIndex,
           selectedItemColor: Colors.amber[800],
@@ -66,4 +62,6 @@ class _NavState extends State<Nav> {
       ),
     );
   }
+
+
 }
