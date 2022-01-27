72c71710bbfdcbfcc0bba7b0f76b3754d7f2c460
guillaumearnx <37373941+guillaumearnx@users.noreply.github.com>
2022-01-27 23:10:14 +0100

    v1 auth

--- charlemiapp-cli/lib/ressources/screens/authscreen.dart
+++ charlemiapp-cli/lib/ressources/screens/authscreen.dart
@@ -6,6 +6,7 @@ import '../nav.dart';
 
 final AuthenticationService _auth = AuthenticationService();
 
+/*Home*/
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
@@ -17,6 +18,7 @@ class HomeScreen extends StatelessWidget {
       );
 }
 
+/*Explore*/
 class ExploreScreen extends StatelessWidget {
   const ExploreScreen({Key? key}) : super(key: key);
 
@@ -28,6 +30,7 @@ class ExploreScreen extends StatelessWidget {
       );
 }
 
+/*Login*/
 class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
 
@@ -81,17 +84,112 @@ class _LoginScreenState extends State<LoginScreen> {
                         setState(() {
                           error = "Invalid username or password";
                           passwordController.clear();
-
                         });
                       }
                     }
                   },
-                  child: const Text('Log in'),
+                  child: const Text('Connexion'),
                 ),
                 ElevatedButton(
-                  child: const Text('Sign up'),
+                    child: const Text('Inscription'),
+                    onPressed: () => setState(() {
+                          authMode = 1;
+                        })),
+                Text(
+                  error,
+                  style: const TextStyle(color: Colors.red, fontSize: 15),
+                ),
+              ],
+            ),
+          ),
+        );
+}
+
+/*Register*/
+class RegisterScreen extends StatefulWidget {
+  const RegisterScreen({Key? key}) : super(key: key);
+
+  @override
+  _RegisterScreenState createState() => _RegisterScreenState();
+}
+
+class AuthScreen extends StatefulWidget {
+  @override
+  _AuthScreenState createState() => _AuthScreenState();
+}
+
+class _AuthScreenState extends State<RegisterScreen> {
+  int authMode = 0;
+
+  @override
+  void initState() {
+    authMode = 0;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return authMode == 0 ? LoginScreen() : RegisterScreen();
+  }
+}
+
+class _RegisterScreenState extends State<RegisterScreen> {
+  TextEditingController usernameController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  String error = "";
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) => Nav.loading
+      ? Loader()
+      : Scaffold(
+          body: Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                TextFormField(
+                  controller: usernameController,
+                  validator: AuthenticationService.validateEmail,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: 'Email',
+                  ),
+                ),
+                const SizedBox(height: 20),
+                TextFormField(
+                  controller: passwordController,
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                    labelText: 'Password',
+                    border: OutlineInputBorder(),
+                  ),
+                  validator: (value) => value!.isEmpty ? "Enter a valid password" : null,
+                ),
+                ElevatedButton(
+                  onPressed: () async {
+                    if (_formKey.currentState!.validate()) {
+                      setState(() {
+                        Nav.loading = true;
+                      });
+                      final String username = usernameController.text;
+                      final String password = passwordController.text;
+                      Nav.user = await _auth.signInWithEmailAndPassword(username, password);
+                      Nav.loading = false;
+                      if (Nav.user == null) {
+                        setState(() {
+                          error = "Invalid username or password";
+                          passwordController.clear();
+                        });
+                      }
+                    }
+                  },
+                  child: const Text('Inscription'),
+                ),
+                ElevatedButton(
+                  child: const Text('Déjà inscrit ?'),
                   onPressed: () => {
-                    //TODO REGISTER
+                    setState(() {
+                      authMode = 0;
+                    })
                   },
                 ),
                 Text(
@@ -104,17 +202,7 @@ class _LoginScreenState extends State<LoginScreen> {
         );
 }
 
-class RegisterScreen extends StatelessWidget {
-  const RegisterScreen({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) => const Scaffold(
-        body: Center(
-          child: Text('Register'),
-        ),
-      );
-}
-
+/*Profile*/
 class ProfileScreen extends StatefulWidget {
   const ProfileScreen({Key? key}) : super(key: key);
 
@@ -123,7 +211,6 @@ class ProfileScreen extends StatefulWidget {
 }
 
 class _ProfileScreenState extends State<ProfileScreen> {
-
   @override
   Widget build(BuildContext context) => Nav.loading
       ? Loader()
