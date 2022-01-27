69fb2c7d899fc30ca152f8bab1bfe2af5cb0ffcb
guillaumearnx <37373941+guillaumearnx@users.noreply.github.com>
2022-01-27 10:31:43 +0100

    wrap in streamprovider

--- charlemiapp-cli/lib/main.dart
+++ charlemiapp-cli/lib/main.dart
@@ -1,15 +1,19 @@
+import 'package:charlemiapp_cli/services/authentication.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-//google font import
-import 'package:google_fonts/google_fonts.dart';
-import 'package:charlemiapp_cli/nav.dart';
+import 'models/user.dart';
+import 'ressources/nav.dart';
 
 const midDarkColor = Color(0xFF1c2031);
 const darkColor = Color(0xFF121421);
 const buttonBlueColor = Color(0xFF4a80ef);
 const whiteColor = Color(0xFFFFFFFF);
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -19,10 +23,10 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: '',
-      home: Nav(),
-    );
+    return StreamProvider<AppUser?>.value(
+        value: AuthenticationService().user,
+        catchError: (_, __) => null,
+        initialData: null,
+        child: const MaterialApp(debugShowCheckedModeBanner: false, title: '', home: Nav()));
   }
 }
