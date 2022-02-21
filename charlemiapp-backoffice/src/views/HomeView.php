770996fd244e0cc089852e9931d52b567e16297a
ARNOUX Guillaume <arnouxguillaume54@hotmail.fr>
2022-02-21 23:57:06 +0100

    commandes

--- charlemiapp-backoffice/src/views/HomeView.php
+++ charlemiapp-backoffice/src/views/HomeView.php
@@ -16,63 +16,71 @@ class HomeView
         $this->container = $container;
     }
 
-    #[Pure] public function render(array $globalOrders): string
+    #[Pure] public function render(array $globalOrders, bool $open): string
     {
+        $btn = $open ? "<button id='chg-btn' name='open' value='close' class=\"openclose close\">Close</button>" : "<button id='chg-btn' name='open' value='open' class=\"openclose open\">Open</button>";
         $columnPending = $columnWaiting = $columnDone = $columnDelivered = $columnCanceled = "";
         foreach ($globalOrders as $status => $orders) {
-            foreach ($orders as $order) {
+            foreach ($orders as $id => $order) {
                 switch ($status) {
                     case "PENDING":
-                        $columnPending .= $this->renderColumn($order);
+                        $columnPending .= $this->renderColumn($order, $id);
                         break;
                     case "WAITING":
-                        $columnWaiting .= $this->renderColumn($order);
+                        $columnWaiting .= $this->renderColumn($order, $id);
                         break;
                     case "DONE":
-                        $columnDone .= $this->renderColumn($order);
+                        $columnDone .= $this->renderColumn($order, $id);
                         break;
                     case "DELIVERED":
-                        $columnDelivered .= $this->renderColumn($order);
+                        $columnDelivered .= $this->renderColumn($order, $id);
                         break;
                     case "CANCELED":
-                        $columnCanceled .= $this->renderColumn($order);
+                        $columnCanceled .= $this->renderColumn($order, $id);
                         break;
                 }
             }
         }
-
         $html = <<<HTML
             <main class="board">
-                <div class="column column-pending" ondrop="drop(event)" ondragover="allowDrop(event)">
+                <div class="column column-pending" data-col-id="pending" ondrop="drop(event)" ondragover="allowDrop(event)">
                     <h2>À valider</h2>
                     $columnPending
                 </div>
-                <div class="column column-waiting" ondrop="drop(event)" ondragover="allowDrop(event)">
+                <div class="column column-waiting" data-col-id="waiting" ondrop="drop(event)" ondragover="allowDrop(event)">
                     <h2>En attente</h2>
                     $columnWaiting
                 </div>
-                <div class="column column-done" ondrop="drop(event)" ondragover="allowDrop(event)">
+                <div class="column column-done" data-col-id="done" ondrop="drop(event)" ondragover="allowDrop(event)">
                     <h2>Prêtes</h2>
                     $columnDone
                 </div>
-                <div class="column column-delivered" ondrop="drop(event)" ondragover="allowDrop(event)">
+                <div class="column column-delivered" data-col-id="delivered" ondrop="drop(event)" ondragover="allowDrop(event)">
                     <h2>Retirées</h2>
                     $columnDelivered
                 </div>
-                <div class="column column-canceled" ondrop="drop(event)" ondragover="allowDrop(event)">
+                <div class="column column-canceled" data-col-id="canceled" ondrop="drop(event)" ondragover="allowDrop(event)">
                     <h2>Annulées</h2>
                     $columnCanceled
                 </div>
             </main>
+            <div class="buttons">
+                <a href="{$this->container['router']->pathFor('logout')}">
+                    <button class="logout">Logout</button>
+                </a>
+                <form action="{$this->container['router']->pathFor('openSet')}" method="POST">
+                    $btn
+                </form>
+                <a href="{$this->container['router']->pathFor('stocks')}">
+                    <button class="stocks">Gestion des stocks</button>
+                </a>
+            </div>
             <script src="/assets/js/draganddrop.js"></script>
-            <a href="{$this->container['router']->pathFor('logout')}">Logout</a>
+            <script src="/assets/js/xhr-open.js"></script>
         </body>
         </html>
         HTML;
-        return genererHeader("Home - CharleMi'App", ["draganddrop.css"]) . $html;
-
-
-        //return  $html;
+        return genererHeader("Home - CharleMi'App", ["home.css", "draganddrop.css"]) . $html;
     }
 
     #[Pure] public function noOrders(): string
@@ -84,12 +92,12 @@ class HomeView
         HTML;
     }
 
-    private function renderColumn(array $order): string
+    private function renderColumn(array $order, string $id): string
     {
         $this::$NB_ORDERS += 1;
         $date = date_format(date_create($order['timestamp'] ?? ""), 'H:i:s');
         return <<<HTML
-        <article class="card" draggable="true" ondragstart="drag(event)" data-id="1">
+        <article class="card" draggable="true" ondragstart="drag(event)" data-id="$id">
                         <h3>Commande {$this::$NB_ORDERS}</h3>
                         <h3>$date</h3>
                     </article>
