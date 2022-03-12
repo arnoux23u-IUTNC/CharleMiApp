c28a9e1bd503d98144193dd8e3f28068ea131619
unshade <72690981+unshade@users.noreply.github.com>
2022-03-12 16:25:00 +0100

    confirmation commander button

--- charlemiapp-cli/lib/ressources/screens/confirmation.dart
+++ charlemiapp-cli/lib/ressources/screens/confirmation.dart
@@ -19,9 +19,52 @@ class Confirmation extends StatefulWidget {
 class _ConfirmationState extends State<Confirmation> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       appBar: AppBarBack(),
       backgroundColor: darkColor,
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: Container(
+              height: MediaQuery.of(context).size.height,
+              color: darkColor,
+              child: SingleChildScrollView(
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [],
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Container(
+              padding: const EdgeInsets.only(left: 55, right: 55, top: 30, bottom: 15),
+              width: double.infinity,
+              child: Column(
+                children: [
+                  const Padding(padding: EdgeInsets.only(top: 10)),
+                  SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      onPressed: () => {
+                        // TODO Trigger commande
+                      },
+                      child: Text(
+                        'Commander',
+                        style: GoogleFonts.poppins(color: Colors.white, fontSize: 15, fontWeight: FontWeight.w600),
+                      ),
+                      style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all(buttonBlueColor),
+                          padding: MaterialStateProperty.all(const EdgeInsets.all(17)),
+                          shape: MaterialStateProperty.all(RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)))),
+                    ),
+                  )
+                ],
+              ))
+        ],
+      ),
     );
   }
 }
